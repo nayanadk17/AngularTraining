bfa6b4a6ed14a454158cfa8b7afcd3435367c0e0
nayanadk17 <96103651+nayanadk17@users.noreply.github.com>
2021-12-20 18:37:48 +0530

    Add files via upload

---
+++ Angular/Day 1/Product.ts
@@ -0,0 +1,7 @@
+export class Product{
+    pid:number=0;
+    pname:string="";
+    price:number=0;
+    category:string="";
+    quantity:number=0;
+}
---
+++ Angular/Day 1/Technology.ts
@@ -0,0 +1,7 @@
+export class Technology{
+
+    public path:string="";
+    public title:string="";
+    public description:string="";
+
+}
